a042224845720d696c68e5105ab6fd112c00afae
aligredo <aliahmedismail37@gmail.com>
2022-03-27 09:34:45 +0200

    schdeuled jobs

--- api/utils/scheduled.js
+++ api/utils/scheduled.js
@@ -2,7 +2,7 @@ var cron = require('node-cron'),
     moment = require('moment'),
     urlcheckController = require('../controllers/urlcheck.controller');
 
-    for (let minute = 1; minute <= 1; minute++) {
+    for (let minute = 1; minute <= 60; minute++) {
         cron.schedule(`*/${minute} * * * *`, () => {
             console.log('\x1b[34m%s\x1b[0m', moment().format('MMMM Do YYYY, h:mm:ss a') + ' started url monitoring batch job for ' + minute + '-minute interval urls.\n');
             urlcheckController.runCheckByInterval(minute);
